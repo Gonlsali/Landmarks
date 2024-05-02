df9338e6fba63a558185eebd23a9ecbc41565782
Gonlsali <radhita09@gmail.com>
2024-05-02 23:20:54 +0700

    "Building List & Navigation" Section 3 & 4 Done

---
+++ Landmarks/View/LandmarksList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksList.swift
+//  Landmarks
+//
+//  Created by Radhita Keniten on 02/05/24.
+//
+
+import SwiftUI
+
+struct LandmarksList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarksList()
+}
