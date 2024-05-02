3437d0ab6f9333293de9ef9574e016688b3070fe
Gonlsali <radhita09@gmail.com>
2024-05-02 18:57:31 +0700

    Chapter 1 All Section Done. Still don't know why to add this line of code bellow. @available(iOS 17.0, *)

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 @main
+@available(iOS 17.0, *)
 struct LandmarksApp: App {
     var body: some Scene {
         WindowGroup {
--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -6,13 +6,23 @@
 //
 
 import SwiftUI
+import MapKit
 
+@available(iOS 17.0, *)
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(initialPosition: .region(region))
+    }
+
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
+@available(iOS 17.0, *)
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
